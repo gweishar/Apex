1336bcc667e0d56b49bbae66656c151d6a331c23
Guilherme Weishar <guilherme@mbpdeguilherme.localdomain>
2017-04-21 10:57:19 +0200

    Changed the refined mesh script to use 48 cores instead of just 24

--- run_steady_refined_perth.sh
+++ run_steady_refined_perth.sh
@@ -26,7 +26,7 @@
 #BSUB -m mpi-l-bull
 
 ### Request number of slots
-#BSUB -n 24
+#BSUB -n 34
 
 cd $HOME/projects/Apex
-mpirun -n 24 ./Apex-opt -i problems/golden_input/d_steady_refined_perth.i
+mpirun -n 34 ./Apex-opt -i problems/golden_input/d_steady_refined_perth.i
